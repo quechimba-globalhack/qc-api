a6e52fcf99486bc915850cbbf21cde3877dc4da4
jlriiot <ing.jmontana@gmail.com>
2020-05-31 08:36:42 -0500

    Creating query for Experience and Auction

--- src/modules/experience/domain/experience.ts
+++ src/modules/experience/domain/experience.ts
@@ -1,14 +1,58 @@
-import { Hash } from "crypto";
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  Query,
+  Args,
+  ObjectType,
+  Field,
+  Int,
+} from "type-graphql";
 
-export class experience {
-  id: BigInt;
-  bakanId: string;
-  content: Hash;
-  startDate: Date;
-  palces: number;
-  baseValue: BigInt;
-  maxAuctionDate: Date;
+@ObjectType()
+export class Auction {
+  @Field()
+  actn_id: number;
+  @Field()
+  exp_id: number;
+  @Field()
+  start_date: Date;
+  @Field()
+  duration: number;
+  @Field()
+  start_value: string;
+  @Field()
+  highest_bid: number;
+  @Field()
+  bkn: string;
+  @Field()
+  req_cancel: number;
+  @Field()
+  created_at: Date;
+}
+
+@ObjectType()
+export class Experience {
+  @Field()
+  exp_id: number;
+  @Field()
+  bkn_id: string;
+  @Field()
+  content: string;
+  @Field()
+  start_date: string;
+  @Field()
+  places: number;
+  @Field()
+  base_val: string;
+  @Field()
+  maxactntdate: Date;
+  @Field()
   engagement: number;
-  publishPrice: number;
-  sealed: boolean;
+  @Field()
+  pub_price: number;
+  @Field()
+  sealed: number;
+  @Field((type) => Auction)
+  actn: Auction;
 }
